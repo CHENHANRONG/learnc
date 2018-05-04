608bba4b1dc338824920ac57d439ba39fb119594
CHENHANRONG <chenhanrong@gmail.com>
2018-05-04 14:36:22 +0800

    Update string_range.c

--- string_range.c
+++ string_range.c
@@ -19,3 +19,52 @@
 /* what book does:
  * 
  */
+
+/* declear functions */
+
+#define MAX_COLS 20
+#define MAX_INPUT 1000
+
+
+extern int read_column_numbers(int *, int);
+extern void rereange(char *, char const, int , int const *);
+
+/*
+ main function
+ */
+int main(int narg, char** argc){
+    int n_columns;
+    int columns[MAX_COLS];
+    
+    char inputs[MAX_INPUT];
+    char outputs[MAX_INPUT];
+    
+    //call func to read columns
+    n_columns = read_column_numbers(columns, MAX_COLS);
+
+}
+
+/*
+ * read column index
+*/
+int read_column_numbers(int columns[], int max)
+{
+    int num = 0;
+    int ch;
+    
+    //read in the range number pair
+    while(num < max && scanf("%d", &columns[num]) == 1 && columns[num] >= 0)
+    {
+        num++;
+    }
+    
+    //check the range numbers are in pair.
+    if(num % 2 !=0 ){
+        puts("Last column number is not paired");
+        exit(EXIT_FAILURE);
+    }
+    
+    while( (ch=getchar()) != EOF && ch != '\n');
+    
+    return num;
+}
