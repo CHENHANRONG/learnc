dd6c075261104587d41b2f9ee220b037230a4dfe
CHENHANRONG <chenhanrong@gmail.com>
2018-05-16 22:17:54 +0800

    Update my_algorithm_link_list.c

--- my_algorithm_link_list.c
+++ my_algorithm_link_list.c
@@ -1,1 +1,18 @@
 
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node* head;
+
+
+struct node* new_node(int data){
+    struct node* pn=(struct node*)malloc(sizeof(node));
+    if(pn!=NULL){
+        pn->data = data;
+        pn->next = NULL;
+    }
+    return pn;
+}
