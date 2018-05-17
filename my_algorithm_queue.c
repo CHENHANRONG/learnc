07fe0f5ef46130681c0f8b430e9d59a5019fd289
CHENHANRONG <chenhanrong@gmail.com>
2018-05-17 16:38:40 +0800

    Update my_algorithm_queue.c

--- my_algorithm_queue.c
+++ my_algorithm_queue.c
@@ -1,16 +1,23 @@
+#define MAXSIZE 100
+
 int queue_buffer[MAXSIZE];
 int icount = 0;
 int ifront = 0;
 void enqueue(int elem){
     if(icount<100){
-        queue_buffer[icount] = elem;
+        queue_buffer[ (ifront + icount) % MAXSIZE ] = elem;
         icount++;
     }
 }
 int dequeue(void){
     if(icount>0){
+        int elem = queue_buffer[ifront++];
         icount--;
-        return queue_buffer[ifront++];
+        //ifront++;
+        if(ifront==MAXSIZE){
+            ifront = 0;
+        }
+        return elem;
     }else{
         return 0;
     }
