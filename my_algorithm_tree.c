300be0f04f50a90d98544837e672631c53d13dd8
CHENHANRONG <chenhanrong@gmail.com>
2018-05-17 10:37:33 +0800

    Update my_algorithm_tree.c

--- my_algorithm_tree.c
+++ my_algorithm_tree.c
@@ -1,1 +1,30 @@
 
+
+struct tnode{
+    int data;
+    struct tnode* left;
+    struct tnode* right;
+};
+
+/*
+ Traversal (printing,searching):
+ • pre-order: root, left subtree, right subtree
+ • Inorder: left subtree, root, right subtree
+ • post-order: right subtree, right subtree, root
+*/
+
+struct tnode* addnode(struct tnode* root, int data){
+    struct tnode *newnode = NULL;
+    if(root == NULL){
+        /* allocate new node, return this new node as new root */
+        newnode = (struct tnode*)malloc(sizeof(tnode));
+        newnode->data=data;
+        newnode->left = NULL;
+        newnode->right = NULL;
+    }else if( data < root->data ){
+        root->left = addnode(root->left, data);
+    }else{
+        root->right = addnode(root->right, data);
+    }
+    return newnode;
+}
