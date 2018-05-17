230d4ffd57fa64e942fab3641f5b5754f306a622
CHENHANRONG <chenhanrong@gmail.com>
2018-05-17 16:52:48 +0800

    Update my_algorithm_queue.c

--- my_algorithm_queue.c
+++ my_algorithm_queue.c
@@ -22,3 +22,40 @@ int dequeue(void){
         return 0;
     }
 }
+
+
+
+struct s_listnode {
+    int element ;
+    struct s_listnode* pnext ;
+};
+struct s_listnode* queue_buffer = NULL;   //start empty
+struct s_listnode *prear = NULL;
+
+void enqueue(int elem){
+    struct s_listnode *new_node = (struct s_listnode*)malloc(sizeof(struct s_listnode));
+    new_node->element = elem;
+    new_node->pnext = NULL;
+    if(prear){
+        prear->pnext = new_node;
+    }else{  //empty
+        queue_buffer = new_node;
+    }
+    prear=new_node;
+}
+int dequeue(void){
+    if(queue_buffer){
+        struct s_listnode  *pelem = queue_buffer;
+        int elem = queue_buffer->element;
+        if(pelem == prear){  //if reach end
+            prear = NULL;
+            queue_buffer = NULL;
+        }else{
+            queue_buffer = pelem->pnext;
+        }
+        free(pelem);
+        return elem;
+    }else{
+        return 0;
+    }
+}
