111355bd121121d6f4ff170356dabf47c04e8d0b
CHENHANRONG <chenhanrong@gmail.com>
2018-05-21 16:34:21 +0800

    Update my_algorithm_tree.c

--- my_algorithm_tree.c
+++ my_algorithm_tree.c
@@ -28,3 +28,57 @@ struct tnode* addnode(struct tnode* root, int data){
     }
     return newnode;
 }
+
+
+////////////////////////////////////////////////////////
+
+struct binary_tree_node{
+    char *node_str;
+    unsigned int count;
+    struct binary_tree_node *lchild;  //left child
+    struct binary_tree_node *rchild;  //right child
+};
+typedef struct binary_tree_node btnode;
+
+
+btnode * malloc_new_btnode(char *instr){
+    btnode *new_node = (btnode*)malloc(sizeof(btnode));
+    if(new_node == NULL){
+        //handle malloc exception
+    }else{
+        new_node->node_str = instr;
+        new_node->count = 1;
+        new_node->node_str = instr;
+        new_node->lchild = NULL;
+        new_node->rchild = NULL;
+    }
+    return new_node;
+}
+
+btnode * add_btnode(btnode *nodex, char *instr){
+  
+    if(instr == NULL){
+        //exception handle
+        return NULL;
+    }
+    else{  // if instr is not NULL
+        if(nodex == NULL){
+            //new node shoud be added
+            nodex = malloc_new_btnode(instr);
+        }else{
+            //if xnode is not NULL
+            char *nstr = nodex->node_str;  // node text
+            int cmp = strcmp(instr, nstr);  //str compare
+            if( cmp == 0){  //find node equal to input str
+                nodex->count++;
+            }else if(strcmp(instr, nstr)<0){  // instr < current nodestr, do in lchild tree
+                nodex->lchild = add_btnode(nodex->lchild, instr); //recursivly do in left subtree
+            }else{  // instr > current nodestr, do in right child tree
+                nodex->rchild = add_btnode(nodex->rchild, instr); //recursivly do in left subtree
+            }
+        }
+    }
+    
+    //not work as expected, return NULL
+    return nodex;
+}
