7a22deae1e14e320844d2640d9f9f8a3c312bbc0
CHENHANRONG <chenhanrong@gmail.com>
2018-05-30 15:51:34 +0800

    Create race.cpp

---
+++ race.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+# include <cstdio>
+//#include <cstring>
+#include "race.cpp"
+#include <pthread.h>
+
+using namespace std;
+
+
+unsigned int cnt = 0;
+
+void *count(void *arg)  //thread body
+{
+    int i;
+    for(i=0;i<10000;i++){
+        cnt++;
+    }
+    return NULL;
+}
+
+
+//==============================================================================
+int main ( int argc , char *argv [ ] )
+{
+    pthread_t tids[4];
+    int i;
+    for(i = 0;i<4; i++){
+        pthread_create(&tids[i], NULL, count, NULL);
+    }
+    
+    for(i=0;i<4;i++){
+        pthread_join(tids[i],NULL);
+    }
+    
+    printf("cnt=%u\n", cnt);
+    
+    return EXIT_SUCCESS;
+}
