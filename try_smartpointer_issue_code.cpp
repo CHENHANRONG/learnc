714ce5483c01883eb520f3f7310401965d0f810f
CHENHANRONG <chenhanrong@gmail.com>
2018-06-28 16:12:15 +0800

    raw pointer memory leak
    raw pointer memory leak example.

---
+++ try_smartpointer_issue_code.cpp
@@ -0,0 +1,32 @@
+//
+//  try_smartpointer_issue_code.cpp
+//  learnc
+//
+//  Created by hanrong chen on 6/28/18.
+//  Copyright © 2018 hanrong chen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    void someMethod() {
+        
+        int *ptr = new int();
+        *ptr = 100;
+        int result = *ptr / 0;  //division by zero error expected
+        delete ptr;  //memory leak, using valgrind --leak-check=full --show-leak-kinds=all ./try_smartpointer_issue_code
+to check
+        cout<<"result="<<result<<endl;
+    }
+};
+
+int main ( ) {
+    
+    MyClass objMyClass;
+    objMyClass.someMethod();
+    
+    return 0;
+    
+}
