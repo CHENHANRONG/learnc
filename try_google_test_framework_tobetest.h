0409ea397d67adfd9b979abc6e5acce56194e29b
CHENHANRONG <chenhanrong@gmail.com>
2018-07-13 17:06:35 +0800

    Create try_google_test_framework_tobetest.h

---
+++ try_google_test_framework_tobetest.h
@@ -0,0 +1,17 @@
+//
+//  try_google_test_framework_tobetest.h
+//  learnc
+//
+//  Created by hanrong chen on 7/13/18.
+//  Copyright © 2018 hanrong chen. All rights reserved.
+//
+
+#ifndef try_google_test_framework_tobetest_h
+#define try_google_test_framework_tobetest_h
+
+class Math{
+public:
+    int add(int,int);
+};
+
+#endif /* try_google_test_framework_tobetest_h */
