f500c6c5ea5ac25dcbcd634ecbca9f41bdce4f28
CHENHANRONG <chenhanrong@gmail.com>
2018-07-13 17:05:47 +0800

    Create try_google_test_framework_tobetest.cpp

---
+++ try_google_test_framework_tobetest.cpp
@@ -0,0 +1,15 @@
+//
+//  try_google_test_framework_tobetest.cpp
+//  learnc
+//
+//  Created by hanrong chen on 7/13/18.
+//  Copyright © 2018 hanrong chen. All rights reserved.
+//
+
+#include <stdio.h>
+#include "try_google_test_framework_tobetest.h"
+
+
+int Math::add(int firstInput, int secondInput){
+    return firstInput+secondInput;
+}
