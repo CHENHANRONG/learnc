aec2d0bb199e76c972145c55b6ed9a04c46662a0
CHENHANRONG <chenhanrong@gmail.com>
2018-07-18 17:16:18 +0800

    Update try_google_test_framework_test.cpp

--- try_google_test_framework_test.cpp
+++ try_google_test_framework_test.cpp
@@ -2,15 +2,14 @@
 //  try_google_test_framework_test.cpp
 //  learnc
 //
-// g++ -isystem /usr/local/include/gtest/ -pthread try_google_test_framework_tobetest.cpp try_google_test_framework_test.cpp /Users/hanrchen/workspace/cpp/libgtest.a -o try_google_test_framework_test
-//
-//  Created by hanrong chen on 7/13/18.
+//  Created by hanrong chen on 7/16/18.
 //  Copyright © 2018 hanrong chen. All rights reserved.
 //
 
-#include <stdio.h>.
+
+//#include <stdio.h>.
 #include <gtest/gtest.h>
-#include "try_google_test_framework_tobetest.h"
+#include "try_google_test_framework_tobetest.hpp"
 
 /* using a TEST macro that takes two parameters.
  The first parameter, namely <XXXTest>, represents the test module name
@@ -18,10 +17,18 @@
  Test modules help us group a bunch of related test cases under a module.
  Hence, it is very important to name the test module and test case
  aptly to improve the readability of the test report.  */
+
+TEST(RPNCalculatorTest, testSimpleAddition ){
+    RPNCalculator rpnCalculator;
+    double actualResult = rpnCalculator.evaluate ( "10 15 +" );
+    double expectedResult = 25.0;
+    EXPECT_EQ ( expectedResult, actualResult ); 
+}
+
 TEST(MathTest, testAdd){
     Math math;
     int expectedVal = 100;
     int actualVal = math.add(70, 30);
-    
-//    EXPECT_EQ(expectedVal, actualVal);
+
+    EXPECT_EQ(expectedVal, actualVal);
 }
