3021c133294357bb45284afa7f92ef021fe4a6c3
CHENHANRONG <chenhanrong@gmail.com>
2018-07-18 17:15:16 +0800

    Update try_google_test_framework_tobetest.cpp

--- try_google_test_framework_tobetest.cpp
+++ try_google_test_framework_tobetest.cpp
@@ -2,14 +2,17 @@
 //  try_google_test_framework_tobetest.cpp
 //  learnc
 //
-//  Created by hanrong chen on 7/13/18.
+//  Created by hanrong chen on 7/16/18.
 //  Copyright © 2018 hanrong chen. All rights reserved.
 //
 
-#include <stdio.h>
-#include "try_google_test_framework_tobetest.h"
+#include "try_google_test_framework_tobetest.hpp"
 
 
 int Math::add(int firstInput, int secondInput){
     return firstInput+secondInput;
+}
+
+double RPNCalculator::evaluate ( string rpnMathExpression ) {
+    return 0.0;
 }
