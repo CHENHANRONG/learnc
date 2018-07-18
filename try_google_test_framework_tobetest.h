191135e2b0d843c4e746bbec5cc706b15dfe983c
CHENHANRONG <chenhanrong@gmail.com>
2018-07-18 17:15:53 +0800

    Update try_google_test_framework_tobetest.h

--- try_google_test_framework_tobetest.h
+++ try_google_test_framework_tobetest.h
@@ -1,17 +1,26 @@
 //
-//  try_google_test_framework_tobetest.h
+//  try_google_test_framework_tobetest.hpp
 //  learnc
 //
-//  Created by hanrong chen on 7/13/18.
+//  Created by hanrong chen on 7/16/18.
 //  Copyright © 2018 hanrong chen. All rights reserved.
 //
 
-#ifndef try_google_test_framework_tobetest_h
-#define try_google_test_framework_tobetest_h
+#ifndef try_google_test_framework_tobetest_hpp
+#define try_google_test_framework_tobetest_hpp
+
+#include <string>
+using namespace std;
 
 class Math{
 public:
     int add(int,int);
 };
 
-#endif /* try_google_test_framework_tobetest_h */
+class RPNCalculator {
+public:
+    double evaluate ( string );
+};
+
+
+#endif /* try_google_test_framework_tobetest_hpp */
