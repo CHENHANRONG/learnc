3d9c84ef8642a244c3e777f9e7c948ce305c4d34
CHENHANRONG <chenhanrong@gmail.com>
2018-07-19 17:11:31 +0800

    Update try_google_test_framework_tobetest.cpp

--- try_google_test_framework_tobetest.cpp
+++ try_google_test_framework_tobetest.cpp
@@ -9,10 +9,37 @@
 #include "try_google_test_framework_tobetest.hpp"
 
 
+
 int Math::add(int firstInput, int secondInput){
     return firstInput+secondInput;
 }
 
-double RPNCalculator::evaluate ( string rpnMathExpression ) {
-    return 0.0;
+double RPNCalculator::evaluate(string rpnMathExpression) {
+    stack<double> numberStack;
+    stringstream rpnMathTokens(rpnMathExpression);
+    istream_iterator<string> begin(rpnMathTokens);
+    istream_iterator<string> end;
+    vector<string> rpnTokens(begin,end);
+    vector<string>::iterator pos = rpnTokens.begin();
+    
+    double firstNumber, secondNumber, result;
+    
+    while(pos != rpnTokens.end()){
+        cout<<"*pos = "<<*pos<<endl;
+//        printf("*pos = %s\n", (*pos).c_str());
+        if(*pos == "+"){
+            firstNumber = numberStack.top();
+            numberStack.pop();
+            secondNumber = numberStack.top();
+            numberStack.pop();
+            result = firstNumber + secondNumber;
+            numberStack.push(result);
+        }else{
+            numberStack.push(std::stod(*pos));
+        }
+        
+        ++pos;
+    }
+    
+    return numberStack.top();
 }
