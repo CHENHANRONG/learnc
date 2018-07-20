0919d517117c88ccac1ee2ac9952f45dc260e2a7
CHENHANRONG <chenhanrong@gmail.com>
2018-07-20 16:47:30 +0800

    Update try_google_test_framework_tobetest.cpp

--- try_google_test_framework_tobetest.cpp
+++ try_google_test_framework_tobetest.cpp
@@ -1,49 +1,70 @@
 //
-//  try_google_test_framework_test.cpp
+//  try_google_test_framework_tobetest.cpp
 //  learnc
 //
 //  Created by hanrong chen on 7/16/18.
 //  Copyright © 2018 hanrong chen. All rights reserved.
 //
 
-
-//#include <stdio.h>.
-#include <gtest/gtest.h>
 #include "try_google_test_framework_tobetest.hpp"
 
-/* using a TEST macro that takes two parameters.
- The first parameter, namely <XXXTest>, represents the test module name
- and the second parameter is the name of the test case.
- Test modules help us group a bunch of related test cases under a module.
- Hence, it is very important to name the test module and test case
- aptly to improve the readability of the test report.  */
 
-TEST(RPNCalculatorTest, testEvaluate){
-    RPNCalculator rpnCalculator;
-    double actualResult = rpnCalculator.evaluate ("2 5 * 4 + 7 2 - 1 + /");
-    double expectedResult = 2.33;
-    ASSERT_NEAR(expectedResult, actualResult, 4);
+
+int Math::add(int firstInput, int secondInput){
+    return firstInput+secondInput;
 }
 
-
-TEST(RPNCalculatorTest, testAdd){
-    RPNCalculator rpnCalculator;
-    double actualResult = rpnCalculator.evaluate ("10 15 +");
-    double expectedResult = 25.0;
-    EXPECT_EQ ( expectedResult, actualResult ); 
-}
-
-TEST(RPNCalculatorTest, testSub){
-    RPNCalculator rpnCalculator;
-    double actualResult = rpnCalculator.evaluate ("25 10 -");
-    double expectedResult = 15.0;
-    EXPECT_EQ ( expectedResult, actualResult );
-}
-
-TEST(MathTest, testAdd){
-    Math math;
-    int expectedVal = 100;
-    int actualVal = math.add(70, 30);
-
-    EXPECT_EQ(expectedVal, actualVal);
+double RPNCalculator::evaluate(string rpnMathExpression) {
+    stack<double> numberStack;
+    stringstream rpnMathTokens(rpnMathExpression);
+    istream_iterator<string> begin(rpnMathTokens);
+    istream_iterator<string> end;
+    vector<string> rpnTokens(begin,end);
+    vector<string>::iterator pos = rpnTokens.begin();
+    
+    double firstNumber, secondNumber, result;
+    
+    while(pos != rpnTokens.end()){
+        cout<<"*pos = "<<*pos<<endl;
+//        printf("*pos = %s\n", (*pos).c_str());
+        if(*pos == "+"){
+            firstNumber = numberStack.top();
+            numberStack.pop();
+            secondNumber = numberStack.top();
+            numberStack.pop();
+            result = firstNumber + secondNumber;
+            numberStack.push(result);
+        }else if(*pos == "*"){
+            firstNumber = numberStack.top();
+            numberStack.pop();
+            secondNumber = numberStack.top();
+            numberStack.pop();
+            result = firstNumber * secondNumber;
+            numberStack.push(result);
+        }else if(*pos == "-"){
+            firstNumber = numberStack.top();
+            numberStack.pop();
+            secondNumber = numberStack.top();
+            numberStack.pop();
+            result = secondNumber - firstNumber;
+            numberStack.push(result);
+        }else if(*pos == "/"){
+            firstNumber = numberStack.top();
+            numberStack.pop();
+            secondNumber = numberStack.top();
+            numberStack.pop();
+            if(firstNumber - 0.0< 0.00000000001){
+                fprintf(stderr,"Dividen number is zero\n");
+            }else{
+                result = secondNumber / firstNumber;
+                numberStack.push(result);
+            }
+        }else{
+            numberStack.push(std::stod(*pos));
+        }
+        
+        ++pos;
+    }
+    
+    return numberStack.top();
 }
