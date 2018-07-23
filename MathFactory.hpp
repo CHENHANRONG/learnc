23716b18cffa84658eed900e6e1a6457f113f389
CHENHANRONG <chenhanrong@gmail.com>
2018-07-23 16:01:29 +0800

    Update MathFactory.hpp

--- MathFactory.hpp
+++ MathFactory.hpp
@@ -1,29 +1,31 @@
 //
-//  MathFactory.cpp
+//  MathFactory.hpp
 //  learnc
 //
 //  Created by hanrong chen on 7/23/18.
 //  Copyright © 2018 hanrong chen. All rights reserved.
 //
 
-#include "MathFactory.hpp"
+#ifndef MathFactory_hpp
+#define MathFactory_hpp
+
+#include <string>
+#include <map>
+using namespace std;
+
+#include "MathOperator.hpp"
+#include "Add.hpp"
+#include "Subtract.h"
+#include "Multiply.h"
+#include "Divide.h"
 
 
+class MathFactory: public MathOperator {
+private:
+    static map<string, MathOperator*> *static_init_block();
+    static map<string, MathOperator*> *pStringToMathObjectMap;
+public:
+    static MathOperator* getMathObject(string strMathOperator="");
+};
 
-map<string, MathOperator*>* MathFactory::static_init_block(){
-    map<string, MathOperator*> *pStringToMathObjectMap = new map<string,MathOperator*>();
-    
-    pStringToMathObjectMap->insert(make_pair("+", new Add()));
-    pStringToMathObjectMap->insert(make_pair("-", new Subtract()));
-    pStringToMathObjectMap->insert(make_pair("*", new Multiply()));
-    pStringToMathObjectMap->insert(make_pair("/", new Divide()));
-    
-    return pStringToMathObjectMap;
-}
-
-map<string,MathOperator*> * MathFactory::pStringToMathObjectMap = static_init_block();
-
-
-MathOperator* MathFactory::getMathObject (string typeOfMathObjectRequired) {
-    return pStringToMathObjectMap->find(typeOfMathObjectRequired)->second;
-}
+#endif /* MathFactory_hpp */
