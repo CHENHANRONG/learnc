df8b8864179fb8c646cec0af3b54f7bdf177a480
CHENHANRONG <chenhanrong@gmail.com>
2018-07-23 15:58:53 +0800

    Update try_google_test_framework_tobetest.cpp

--- try_google_test_framework_tobetest.cpp
+++ try_google_test_framework_tobetest.cpp
@@ -7,7 +7,7 @@
 //
 
 #include "try_google_test_framework_tobetest.hpp"
-
+#include "MathFactory.hpp"
 
 
 int Math::add(int firstInput, int secondInput){
@@ -15,6 +15,8 @@ int Math::add(int firstInput, int secondInput){
 }
 
 double RPNCalculator::evaluate(string rpnMathExpression) {
+    MathOperator *mathOp = MathFactory::getMathObject();
+    
     stack<double> numberStack;
     stringstream rpnMathTokens(rpnMathExpression);
     istream_iterator<string> begin(rpnMathTokens);
@@ -25,46 +27,63 @@ double RPNCalculator::evaluate(string rpnMathExpression) {
     double firstNumber, secondNumber, result;
     
     while(pos != rpnTokens.end()){
-        cout<<"*pos = "<<*pos<<endl;
+//        cout<<"*pos = "<<*pos<<endl;
 //        printf("*pos = %s\n", (*pos).c_str());
-        if(*pos == "+"){
-            firstNumber = numberStack.top();
-            numberStack.pop();
+        if(isMathOperator(*pos)){
             secondNumber = numberStack.top();
             numberStack.pop();
+            firstNumber = numberStack.top();
+            numberStack.pop();
+            
+            pMathOperator = MathFactory::getMathOperator(*pos);
+            result = pMathOperator->evaluate(firstNumber, secondNumber);
             result = firstNumber + secondNumber;
             numberStack.push(result);
-        }else if(*pos == "*"){
-            firstNumber = numberStack.top();
-            numberStack.pop();
-            secondNumber = numberStack.top();
-            numberStack.pop();
-            result = firstNumber * secondNumber;
-            numberStack.push(result);
-        }else if(*pos == "-"){
-            firstNumber = numberStack.top();
-            numberStack.pop();
-            secondNumber = numberStack.top();
-            numberStack.pop();
-            result = secondNumber - firstNumber;
-            numberStack.push(result);
-        }else if(*pos == "/"){
-            firstNumber = numberStack.top();
-            numberStack.pop();
-            secondNumber = numberStack.top();
-            numberStack.pop();
-            if(firstNumber - 0.0< 0.00000000001){
-                fprintf(stderr,"Dividen number is zero\n");
-            }else{
-                result = secondNumber / firstNumber;
-                numberStack.push(result);
-            }
         }else{
             numberStack.push(std::stod(*pos));
         }
         
         ++pos;
     }
+        
+//        if(*pos == "+"){
+//            firstNumber = numberStack.top();
+//            numberStack.pop();
+//            secondNumber = numberStack.top();
+//            numberStack.pop();
+//            result = firstNumber + secondNumber;
+//            numberStack.push(result);
+//        }else if(*pos == "*"){
+//            firstNumber = numberStack.top();
+//            numberStack.pop();
+//            secondNumber = numberStack.top();
+//            numberStack.pop();
+//            result = firstNumber * secondNumber;
+//            numberStack.push(result);
+//        }else if(*pos == "-"){
+//            firstNumber = numberStack.top();
+//            numberStack.pop();
+//            secondNumber = numberStack.top();
+//            numberStack.pop();
+//            result = secondNumber - firstNumber;
+//            numberStack.push(result);
+//        }else if(*pos == "/"){
+//            firstNumber = numberStack.top();
+//            numberStack.pop();
+//            secondNumber = numberStack.top();
+//            numberStack.pop();
+//            if(firstNumber - 0.0< 0.00000000001){
+//                fprintf(stderr,"Dividen number is zero\n");
+//            }else{
+//                result = secondNumber / firstNumber;
+//                numberStack.push(result);
+//            }
+//        }else{
+//            numberStack.push(std::stod(*pos));
+//        }
+//
+//        ++pos;
+//    }
     
     return numberStack.top();
 }
