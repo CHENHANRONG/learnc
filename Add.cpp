974752a3bf6586a954d84ac551248df1139b2950
CHENHANRONG <chenhanrong@gmail.com>
2018-07-23 16:03:49 +0800

    Create Add.cpp

---
+++ Add.cpp
@@ -0,0 +1,14 @@
+//
+//  Add.cpp
+//  learnc
+//
+//  Created by hanrong chen on 7/23/18.
+//  Copyright © 2018 hanrong chen. All rights reserved.
+//
+
+#include "Add.hpp"
+
+
+double Add::evaluate(int firstInput,int secondInput) {
+    return firstInput + secondInput;
+}
