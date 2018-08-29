c481e5e5d8fae0604f4e30f6510c35faa2a389da
hanrong chen <93312048@qq.com>
2018-08-29 19:13:12 +0800

    changed

---
+++ Algorithm_Sort.cpp
@@ -0,0 +1,22 @@
+
+namespace mylib
+{
+
+template <typename elemT>
+class Sort
+{
+    void bubble_sort()
+    {
+    }
+
+    /* first version of bubble sort */
+    void bubble_sort_v1()
+    {
+    }
+
+    void quick_sort()
+    {
+    }
+};
+
+} // namespace mylib
