387fad63e0f56349e36e18f1858cf878e9a088b4
CHENHANRONG <chenhanrong@gmail.com>
2018-09-04 22:51:25 +0800

    Create string_circular_shift.cpp

---
+++ algorithm/codes/string_circular_shift.cpp
@@ -0,0 +1,28 @@
+/*
+A string s is a circular shift (or circular rotation) of a string t if it matches when the characters are circularly shifted by any number of positions; 
+e.g., ACTGACG is a circular shift of TGACGAC, and vice versa. 
+Detecting this condition is important in the study of genomic sequences. 
+Write a program that checks whether two given strings s and t are circular shifts of one another. 
+Hint: The solution is a one-liner with indexOf(), length(), and string concatenation.
+*/
+
+/*
+My solution:
+*/
+
+
+
+
+/*
+Better solutions:
+*/
+
+
+/*
+Best solutions:
+*/
+
+
+/*
+Review:
+*/
