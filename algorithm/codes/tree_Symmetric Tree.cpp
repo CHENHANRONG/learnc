5278dbec34b4f7aadf4a9f6ae9a9733830c0ab23
CHENHANRONG <chenhanrong@gmail.com>
2018-09-25 20:00:38 +0800

    Update tree_Symmetric Tree.cpp

--- algorithm/codes/tree_Symmetric Tree.cpp
+++ algorithm/codes/tree_Symmetric Tree.cpp
@@ -25,10 +25,29 @@ Bonus points if you could solve it both recursively and iteratively.
 Java 
 */
 
-/**
+    /**
      * @param root: root of the given tree
      * @return: whether it is a mirror of itself
      */
+     //正确的
+     public boolean isSymmetric(TreeNode root) {
+            if(root == null)
+                return true;
+                
+            return helper(root.left, root.right);
+
+        }
+        
+        public boolean helper(TreeNode left, TreeNode right){
+            if(left == null && right == null)
+                return true;
+            else if(left == null || right == null)
+                return false;
+            else
+                return helper(left.left, right.right) && helper(left.right, right.left);
+        }
+        
+     //有问题的
     public static boolean isSymmetric(TreeNode root) {
         // Write your code here
         if(root == null)
