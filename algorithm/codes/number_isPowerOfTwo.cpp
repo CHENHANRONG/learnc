76766d031491671c7ac78fbf2ffd44c4966b381b
CHENHANRONG <chenhanrong@gmail.com>
2018-09-25 20:22:44 +0800

    Update number_isPowerOfTwo.cpp

--- algorithm/codes/number_isPowerOfTwo.cpp
+++ algorithm/codes/number_isPowerOfTwo.cpp
@@ -23,3 +23,19 @@ Given an integer, write a function to determine if it is a power of two.
         
         return true;
     }
+
+/*
+解题思路:
+如果一个数 n 是 2 的整数幂，那么以二进制表示的话只有一位是 1 ，其余每一位都是 0 ，而 n - 1 则全是 1 ，且比 n 少一位最高位，两者相与为零
+*/
+    /*
+     * @param n: An integer
+     * @return: True or false
+     */
+    public boolean checkPowerOf2(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        return (n & (n - 1)) == 0;
+    }
