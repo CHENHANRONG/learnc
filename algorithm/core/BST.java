db965692d1c36d46692c1245733654f9546ad64e
CHENHANRONG <chenhanrong@gmail.com>
2018-09-26 20:21:03 +0800

    Update BST.java

--- algorithm/core/BST.java
+++ algorithm/core/BST.java
@@ -11,4 +11,20 @@ public class BST<Key extends Comparable<Key>, Value>
         private Node left, right;   //指向子树的链接
         private int N;    //以该节点为根的子树中的节点总数
         
+        public Node(Key key, Value val, int N)
+        {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
     }
+    
+    pubic int size()
+    {
+        return size(root);
+    }
+    
+    
+}
+    
+    
