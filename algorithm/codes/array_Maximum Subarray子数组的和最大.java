915b721fc96abffa51b913ecf4e6c90704b15fdb
CHENHANRONG <chenhanrong@gmail.com>
2018-09-27 22:13:32 +0800

    Update array_Maximum Subarray子数组的和最大.java

--- algorithm/codes/array_Maximum Subarray子数组的和最大.java
+++ algorithm/codes/array_Maximum Subarray子数组的和最大.java
@@ -14,6 +14,36 @@ If you have figured out the O(n) solution, try coding another solution using the
 
 
 /*
+better solution:
+
+
+
+this problem was discussed by Jon Bentley (Sep. 1984 Vol. 27 No. 9 Communications of the ACM P885)
+
+the paragraph below was copied from his paper (with a little modifications)
+
+algorithm that operates on arrays: it starts at the left end (element A[1]) and scans through to the right end (element A[n]), keeping track of the maximum sum subvector seen so far. The maximum is initially A[0]. Suppose we've solved the problem for A[1 .. i - 1]; how can we extend that to A[1 .. i]? The maximum
+sum in the first I elements is either the maximum sum in the first i - 1 elements (which we'll call MaxSoFar), or it is that of a subvector that ends in position i (which we'll call MaxEndingHere).
+
+MaxEndingHere is either A[i] plus the previous MaxEndingHere, or just A[i], whichever is larger.
+
+*/
+
+
+
+public static int maxSubArray(int[] A) {
+    int maxSoFar=A[0], maxEndingHere=A[0];
+    for (int i=1;i<A.length;++i){
+    	maxEndingHere= Math.max(maxEndingHere+A[i],A[i]);
+    	maxSoFar=Math.max(maxSoFar, maxEndingHere);	
+    }
+    return maxSoFar;
+}
+
+
+
+
+/*
 My very begining solution:
 思路：初始化max和tSum两个变量，max记录最大的子数组tSum值， tSum用来记录阶段的子数组和。
 当访问到某个位置i时，
@@ -49,22 +79,3 @@ My very begining solution:
         return preMax;
     }
 
-
-
-this problem was discussed by Jon Bentley (Sep. 1984 Vol. 27 No. 9 Communications of the ACM P885)
-
-the paragraph below was copied from his paper (with a little modifications)
-
-algorithm that operates on arrays: it starts at the left end (element A[1]) and scans through to the right end (element A[n]), keeping track of the maximum sum subvector seen so far. The maximum is initially A[0]. Suppose we've solved the problem for A[1 .. i - 1]; how can we extend that to A[1 .. i]? The maximum
-sum in the first I elements is either the maximum sum in the first i - 1 elements (which we'll call MaxSoFar), or it is that of a subvector that ends in position i (which we'll call MaxEndingHere).
-
-MaxEndingHere is either A[i] plus the previous MaxEndingHere, or just A[i], whichever is larger.
-
-public static int maxSubArray(int[] A) {
-    int maxSoFar=A[0], maxEndingHere=A[0];
-    for (int i=1;i<A.length;++i){
-    	maxEndingHere= Math.max(maxEndingHere+A[i],A[i]);
-    	maxSoFar=Math.max(maxSoFar, maxEndingHere);	
-    }
-    return maxSoFar;
-}
