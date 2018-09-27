5e25c904c98ff8e8d31fd9d42e494af57a740572
CHENHANRONG <chenhanrong@gmail.com>
2018-09-27 16:30:42 +0800

    Update number_Palindrome Number.java

--- algorithm/codes/number_Palindrome Number.java
+++ algorithm/codes/number_Palindrome Number.java
@@ -20,6 +20,28 @@ Follow up:
 
 Coud you solve it without converting the integer to a string?
 */
+
+
+
+/*
+Better solution
+方法2: 反转前半部，跟后半部比较是否一样
+
+
+Following the thoughts based on the second idea, to avoid the overflow issue of the reverted number, what if we only revert half of the \text{int}int number? After all, the reverse of the last half of the palindrome should be the same as the first half of the number, if the number is a palindrome.
+
+For example, if the input is 1221, if we can revert the last part of the number "1221" from "21" to "12", and compare it with the first half of the number "12", since 12 is the same as 12, we know that the number is a palindrome.
+*/
+
+
+//TODO
+
+
+
+
+/*
+My first solution
+*/
 public boolean isPalindrome(int x) {
         int flag = x<0?-1:1;
         int input = x;
@@ -37,14 +59,4 @@ public boolean isPalindrome(int x) {
             return false;
     }
 
-/*
-方法2: 反转前半部，跟后半部比较是否一样
 
-
-Following the thoughts based on the second idea, to avoid the overflow issue of the reverted number, what if we only revert half of the \text{int}int number? After all, the reverse of the last half of the palindrome should be the same as the first half of the number, if the number is a palindrome.
-
-For example, if the input is 1221, if we can revert the last part of the number "1221" from "21" to "12", and compare it with the first half of the number "12", since 12 is the same as 12, we know that the number is a palindrome.
-*/
-
-
-//TODO
