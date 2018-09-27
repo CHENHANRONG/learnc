fe16876a214f5ecfb31b48f0f9d653f080fd5089
CHENHANRONG <chenhanrong@gmail.com>
2018-09-27 16:19:00 +0800

    Create number_Palindrome Number.java

---
+++ algorithm/codes/number_Palindrome Number.java
@@ -0,0 +1,38 @@
+/*
+9. Palindrome Number
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Follow up:
+
+Coud you solve it without converting the integer to a string?
+*/
+public boolean isPalindrome(int x) {
+        int flag = x<0?-1:1;
+        int input = x;
+        int res = 0;
+         while(x!=0){
+             int xpos = x %10;
+             x = x/10;
+             
+             res = res*10+xpos;
+         }
+        
+        if(res*flag == input)
+            return true;
+        else 
+            return false;
+    }
