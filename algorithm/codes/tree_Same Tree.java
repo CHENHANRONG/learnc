5df82ec2b994200cfccf82d8d236550edcb11fcf
CHENHANRONG <chenhanrong@gmail.com>
2018-09-28 17:15:39 +0800

    Create tree_Same Tree.java

---
+++ algorithm/codes/tree_Same Tree.java
@@ -0,0 +1,66 @@
+/*
+100. Same Tree
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/*
+my first solution:
+思路： 递归
+左子树&&右子树都相等
+*/
+
+public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p ==null && q==null)
+            return true;
+        else if(p==null || q==null)
+            return false;
+        else{
+            if(p.val != q.val)
+                return false;
+            else
+                return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        }
+        
+    }
