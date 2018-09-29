d341677cd0b3f586f56fd820052d4bcd0f19d354
CHENHANRONG <chenhanrong@gmail.com>
2018-09-29 10:50:56 +0800

    Update list_Add Two Numbers.java

--- algorithm/codes/list_Add Two Numbers.java
+++ algorithm/codes/list_Add Two Numbers.java
@@ -15,6 +15,30 @@ Explanation: 342 + 465 = 807.
 
 
 /*
+更简练都表述
+*/
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = l1, q = l2, curr = dummyHead;
+    int carry = 0;
+    while (p != null || q != null) {
+        int x = (p != null) ? p.val : 0;
+        int y = (q != null) ? q.val : 0;
+        int sum = carry + x + y;
+        carry = sum / 10;
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        if (p != null) p = p.next;
+        if (q != null) q = q.next;
+    }
+    if (carry > 0) {
+        curr.next = new ListNode(carry);
+    }
+    return dummyHead.next;
+}
+
+
+/*
 My init solution:
 思路： 从第一位（个位）开始，遍历每个节点
 如果节点不为null， 获取节点都val； 如果节点为null， 返回0
