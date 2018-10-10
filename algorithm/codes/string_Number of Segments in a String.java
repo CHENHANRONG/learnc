ed2f054279646d0151c249db205f36b1d568b55b
CHENHANRONG <chenhanrong@gmail.com>
2018-10-10 13:24:21 +0800

    Update string_Number of Segments in a String.java

--- algorithm/codes/string_Number of Segments in a String.java
+++ algorithm/codes/string_Number of Segments in a String.java
@@ -28,3 +28,42 @@ use split(String reg);
         }
         return trimmed.split("\\s+").length;
     }
+
+
+/*
+in place solution
+
+Approach #2 In-place [Accepted]
+Intuition
+
+If we cannot afford to allocate linear additional space, a fairly simple algorithm can deduce the number of segments in linear time and constant space.
+
+Algorithm
+
+To count the number of segments, it is equivalent to count the number of string indices at which a segment begins. Therefore, by formally defining the characteristics of such an index, we can simply iterate over the string and test each index in turn. Such a definition is as follows: a string index begins a segment if it is preceded by whitespace (or is the first index) and is not whitespace itself, which can be checked in constant time. Finally, we simply return the number of indices for which the condition is satisfied.
+
+
+Complexity Analysis
+
+Time complexity : O(n)
+
+We do a constant time check for each of the string's nn indices, so the runtime is overall linear.
+
+Space complexity : O(1)
+
+There are only a few integers allocated, so the memory footprint is constant.
+
+*/
+
+    public int countSegments(String s) {
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+
+        return segmentCount;
+    }
+
