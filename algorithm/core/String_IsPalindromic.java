e5dbf3c406a21c1212c2135916ac59324577f4ea
CHENHANRONG <chenhanrong@gmail.com>
2018-10-11 14:03:17 +0800

    判断String是不是回文

---
+++ algorithm/core/String_IsPalindromic.java
@@ -0,0 +1,13 @@
+/*
+判断String是不是回文
+*/
+
+    private boolean isPalin(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++; j--;
+            }
+            else return false;
+        }
+        return true;
+    }
