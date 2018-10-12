1969bacdf5948c4b8eda0635f14a15ed67804d73
CHENHANRONG <chenhanrong@gmail.com>
2018-10-12 16:58:55 +0800

    Update Graph.java

--- algorithm/core/Graph.java
+++ algorithm/core/Graph.java
@@ -23,8 +23,84 @@ public class Graph {
 
     }
     
-    String toString(){
-        
+   public String toString(){
+        String s = V+" vertices,  "+ E+" edges\n";
+        for(int v=0; v<V; v++){
+            s += v + ": ";
+            for(int w : this.adj(v)){
+                s += w + " ";
+            }
+
+            s += "\n";
+        }
+
+        return s;
+    }
+
+
+    /**
+     * Calc vertex's degree
+     * degree: number of edges adhere to this vertex
+     *
+     *
+     * @param G
+     * @param v
+     * @return
+     */
+    public static int degree(Graph G, int v){
+        int degree = 0;
+        for(int w : G.adj(v))
+            degree++;
+
+        return degree;
+    }
+
+
+    /**
+     * Find max degree of graph.
+     * Go through all vertexes and find out the max
+     *
+     * @param G
+     * @return
+     */
+    public static int maxDegree(Graph G){
+        int max = 0;
+        for(int v=0; v<G.V(); v++){
+            if(degree(G, v)>max)
+                max = degree(G, v);
+        }
+
+        return max;
+    }
+
+
+    /**
+     * Calc average degree of the graph
+     * 2*#edges/#vertexes
+     *
+     * @param G
+     * @return
+     */
+    public static double avgDegree(Graph G){
+        return 2.0*G.E()/G.V();
+    }
+
+
+    /**
+     * If vertex v's adj vertex contain itself, then that means there is self loops.
+     * @param G
+     * @return
+     */
+    public static int numberOfSelfLoops(Graph G){
+        int count = 0;
+        for(int v=0; v<G.V(); v++){
+            for(int w : G.adj(v)){
+                if(v == w)
+                    count++;
+            }
+        }
+
+        return count/2;   //every edge count for 2 times.
     }
 
 
