d9ed06c29b00e07e63420d0c3c0ad58d5c9016a8
CHENHANRONG <chenhanrong@gmail.com>
2018-10-12 16:11:32 +0800

    Create Graph.java

---
+++ algorithm/core/Graph.java
@@ -0,0 +1,32 @@
+public class Graph {
+    Graph(int V){
+
+    }
+
+//    Graph(In in){
+//
+//    }
+
+    int V(){  // #vertex
+
+    }
+
+    int E(){  // #edge
+
+    }
+
+    void addEdge(int v, int w){  // add new edge v-w
+
+    }
+
+    Iterable<Integer> adj(int V){  // all vertexes adj to V
+
+    }
+    
+    String toString(){
+        
+    }
+
+
+
+}
