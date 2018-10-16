b7143d87b6c85eb433b9bf27f0bbfe770946d709
CHENHANRONG <chenhanrong@gmail.com>
2018-10-16 21:40:37 +0800

    Update Graph.java

--- algorithm/core/Graph.java
+++ algorithm/core/Graph.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class Graph {
     private final int V;  // #vertexes
@@ -167,7 +165,7 @@ class DepthFirstPaths{
     private int[] edgeTo;
     private final int s;
 
-    public DepthFirstPath(Graph G, int s){
+    public DepthFirstPaths(Graph G, int s){
         this.marked = new boolean[G.V()];
         this.edgeTo = new int[G.V()];
         this.s = s;
@@ -216,4 +214,190 @@ class DepthFirstPaths{
     }
 
 
+}
+
+
+class BreadthFirstPaths {
+    private boolean[] marked;  // Is a shortest path to this vertex known?
+    private int[] edgeTo;  // last vertex on known path to this verte
+    private final int s;  // source
+
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        bfs(G, s);
+    }
+
+    public void bfs(Graph G, int s) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        marked[s] = true;
+        queue.add(s);
+        while (!queue.isEmpty()) {
+            int v = queue.poll();  // Remove next vertex from the queue.
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {  // For every unmarked adjacent vertex
+                    edgeTo[w] = v;  // save last edge on a shortest path
+                    marked[w] = true;
+                    queue.add(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+
+    public Iterable<Integer> pathTo(int v){
+        if(!hasPathTo(v))
+            return null;
+
+        Stack<Integer> path = new Stack();
+        for(int x=v; x!=s; x=this.edgeTo[x]){
+            path.push(x);
+        }
+
+        path.push(s);
+
+        return path;
+    }
+
+}
+
+
+/**
+ * Depth-first search to find connected components in a graph
+ */
+class CC   //connected components
+{
+
+    private boolean[] marked;
+    private int[] id;  // id[i] is vertex i's CC
+    private int count;  // CC's value
+
+    public CC(Graph G){
+        marked = new boolean[G.V()];  // init marked
+        id = new int[G.V()];  // init id
+        for(int s=0; s<G.V(); s++){
+            if(!marked[s]){
+                dfs(G,s);
+                this.count++;  // calc CC value
+            }
+        }
+    }
+
+
+    public void dfs(Graph G, int v)
+    {
+        marked[v] = true;
+        id[v] = this.count;
+        for(int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+
+
+    public boolean connected(int v, int w)
+    {
+        return id[v] == id[w];
+    }
+
+    public int id(int v)
+    {
+        return id[v];
+    }
+
+    public int count()
+    {
+        return count;
+    }
+}
+
+
+/**
+ * is G acyclic? (assumes no self-loops or parallel edges)
+ */
+class Cycle
+{
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph G)
+    {
+        marked = new boolean[G.V()];
+        for (int s = 0; s < G.V(); s++){
+            if (!marked[s])
+                dfs(G, s, s);
+        }
+    }
+
+
+    /**
+     *
+     * @param G : Graph
+     * @param v :
+     * @param u : ???
+     */
+    public void dfs(Graph G, int v, int u)
+    {
+        marked[v] = true;
+        for(int w : G.adj(v)) {
+            if (!marked[w])
+            {
+                dfs(G, w, v);
+            }
+            else if(w != u)  // w is marked, and w is not u  ???
+            {
+                this.hasCycle = true;
+            }
+        }
+    }
+
+    public boolean hasCycle(){
+        return this.hasCycle;
+    }
+
+}
+
+
+/**
+ * is G bipartite? (two-colorable)
+ */
+class TwoColor
+{
+    private boolean[] marked;
+    private boolean[] color;  //
+    private boolean isTwoColorable = true;  //
+
+    public TwoColor(Graph G){
+        marked = new boolean[G.V()];  // init marked
+        color = new boolean[G.V()];  // init color array
+        for(int s=0; s<G.V(); s++){
+            if(!marked[s]){
+                dfs(G,s);
+            }
+        }
+    }
+
+
+    public void dfs(Graph G, int v)
+    {
+        marked[v] = true;
+        for(int w : G.adj(v)) {
+            if (!marked[w]) {
+                color[w] = !color[v];
+                this.dfs(G, w);
+            }else if(color[w] == color[v]){
+                this.isTwoColorable = false;
+            }
+        }
+    }
+
+    public boolean isBipartite(){
+        return this.isTwoColorable;
+    }
 }
