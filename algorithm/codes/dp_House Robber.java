9d4684096011aff086d529a4a90cee3c2eef7b82
CHENHANRONG <chenhanrong@gmail.com>
2018-10-19 17:11:54 +0800

    Update dp_House Robber.java

--- algorithm/codes/dp_House Robber.java
+++ algorithm/codes/dp_House Robber.java
@@ -51,3 +51,7 @@ public int rob(int[] nums) {
 
     return max;
 }
+
+/*
+others: https://leetcode.com/problems/house-robber/discuss/156523/From-good-to-great.-How-to-approach-most-of-DP-problems.
+*/
