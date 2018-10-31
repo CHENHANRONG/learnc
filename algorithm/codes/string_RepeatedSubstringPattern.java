2374ab46474fccaaf25fa5f9c29d77fa1c4a0357
陈含荣 <93312048@qq.com>
2018-10-31 20:23:51 +0800

    string_RepeatedSubstringPattern.java better solution

--- algorithm/codes/string_RepeatedSubstringPattern.java
+++ algorithm/codes/string_RepeatedSubstringPattern.java
@@ -51,3 +51,12 @@ public boolean repeatedSubstringPattern(String s) {
     
     return res;
 }
+
+
+/*
+Amazing solution:
+*/
+public boolean repeatedSubstringPattern(String str) {
+    String s = str + str;
+    return s.substring(1, s.length() - 1).contains(str);
+}
